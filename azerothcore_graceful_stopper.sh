f3155e5ce21ce2b8acbcf8e10274ea4825903062
Abracadaniel222 <178446201+abracadaniel222@users.noreply.github.com>
2025-07-01 15:43:05 +0000

    update

--- azerothcore_graceful_stopper.sh
+++ azerothcore_graceful_stopper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# shutdown AzerothCore using soap
+# Gracefully shutdown AzerothCore using SOAP
 # @author abracadaniel222
 
 SOAP_HOST="127.0.0.1"
@@ -17,23 +17,37 @@ else
 fi
 
 if ! pgrep -u azerothuser worldserver > /dev/null; then
-  # already stopped
+  echo "Already stopped"
   exit 0
 fi
 
 read -r -d '' SOAP_XML <<EOF
 <?xml version="1.0"?>
-<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
-                   SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+<SOAP-ENV:Envelope
+    xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" 
+    xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" 
+    xmlns:xsi="http://www.w3.org/1999/XMLSchema-instance" 
+    xmlns:xsd="http://www.w3.org/1999/XMLSchema" 
+    xmlns:ns1="urn:AC">
   <SOAP-ENV:Body>
-    <ns1:executeCommand xmlns:ns1="urn:AC">
+    <ns1:executeCommand>
       <command>server shutdown $SHUTDOWN_TIMEOUT_SECONDS</command>
     </ns1:executeCommand>
   </SOAP-ENV:Body>
 </SOAP-ENV:Envelope>
 EOF
 
-curl -s -X POST \
-  -H "Content-Type: text/xml" \
-  --data "$SOAP_XML" \
-  "http://$SOAP_USER:$SOAP_PASSWORD@$SOAP_HOST:$SOAP_PORT/"
+response=$(curl -s -w "\n%{http_code}" -X POST "http://$SOAP_HOST:$SOAP_PORT/" -H "Content-Type: text/xml" --user "$SOAP_USER:$SOAP_PASSWORD" -d "$SOAP_XML"  2>&1)
+
+curl_exit=$?
+
+body=$(echo "$response" | sed '$d')
+code=$(echo "$response" | tail -n1)
+
+if [ "$curl_exit" -ne 0 ] || [ "$code" != "200" ]; then
+  echo "SOAP shutdown failed:"
+  echo "HTTP code: ${code:-<none>}"
+  echo "Response:"
+  echo "$body"
+  exit 1
+fi
