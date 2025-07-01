87a34eb98a67e08e755de12f4a8236c60ec41767
abracadaniel222 <178446201+abracadaniel222@users.noreply.github.com>
2025-07-01 10:44:07 -0400

    Update azerothcore_graceful_stopper.sh

--- azerothcore_graceful_stopper.sh
+++ azerothcore_graceful_stopper.sh
@@ -2,17 +2,18 @@
 # shutdown AzerothCore using soap
 # @author abracadaniel222
 
-# Load SOAP credentials from systemd runtime secrets
-SOAP_USER=$(cat /run/cred/ac_soap_user)
-SOAP_PASS=$(cat /run/cred/ac_soap_pass)
-
 SOAP_HOST="127.0.0.1"
 SOAP_PORT="7878"
 SHUTDOWN_TIMEOUT_SECONDS=60
+SOAP_USER="admin"
+SOAP_PASSWORD="admin"
 
 CONF_FILE="$HOME/scripts/azerothcore-graceful-stopper/azerothcore_graceful_stopper.conf"
 if [ -f "$CONF_FILE" ]; then
   source "$CONF_FILE"
+else
+  echo "Missing config file: $CONF_FILE" >&2
+  exit 1
 fi
 
 if ! pgrep -u azerothuser worldserver > /dev/null; then
@@ -35,4 +36,4 @@ EOF
 curl -s -X POST \
   -H "Content-Type: text/xml" \
   --data "$SOAP_XML" \
-  "http://$SOAP_USER:$SOAP_PASS@$SOAP_HOST:$SOAP_PORT/"
+  "http://$SOAP_USER:$SOAP_PASSWORD@$SOAP_HOST:$SOAP_PORT/"
